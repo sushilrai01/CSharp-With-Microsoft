73a15bc3e4285414cd24505c981cf33a4beee9c5
sushilrai01 <ssilray16@gmail.com>
2023-09-20 22:34:49 +0545

    admin level

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -63,17 +63,52 @@
 //     Console.WriteLine(orderID);
 // }
 
-string str = "The quick brown fox jumps over the lazy dog.";
+// string str = "The quick brown fox jumps over the lazy dog.";
 
-char[] charMessage = str.ToCharArray();
-Array.Reverse(charMessage);
-int x = 0;
+// char[] charMessage = str.ToCharArray();
+// Array.Reverse(charMessage);
+// int x = 0;
 
-foreach (char c in charMessage){
-    if (c == 'o') {x++;}
+// foreach (char c in charMessage){
+//     if (c == 'o') {x++;}
+// }
+
+// string new_message = new String(charMessage);
+// // print it out
+// Console.WriteLine(new_message);
+// Console.WriteLine($"'o' appears {x} times.");
+
+// int saleAmount = 101;
+// // int discount = saleAmount > 1000 ? 100 : 50;
+
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
 }
-
-string new_message = new String(charMessage);
-// print it out
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
