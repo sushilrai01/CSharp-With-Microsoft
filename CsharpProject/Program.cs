15b6b39cd148f5880108a2a6cc08b67cac79fa59
sushilrai01 <ssilray16@gmail.com>
2023-09-23 17:49:42 +0545

    Role playing game battle challenge

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -272,18 +272,91 @@
 
 //----------------_______FizzBuzz Challenge_____------
 
-for (int i = 1; i <= 100; i++)
-{
-    if(i % 3 == 0 && i % 5 ==0){
-        Console.WriteLine(i + "- FizzBuzz");
-    }
+// for (int i = 1; i <= 100; i++)
+// {
+//     if(i % 3 == 0 && i % 5 ==0){
+//         Console.WriteLine(i + "- FizzBuzz");
+//     }
    
-    else if (i % 5 == 0){
-        Console.WriteLine(i + "- Buzz");
-    }
-    else if(i % 3 == 0){
-        Console.WriteLine(i + "- Fizz");
-    }
-    else
-        Console.WriteLine(i);
-}
+//     else if (i % 5 == 0){
+//         Console.WriteLine(i + "- Buzz");
+//     }
+//     else if(i % 3 == 0){
+//         Console.WriteLine(i + "- Fizz");
+//     }
+//     else
+//         Console.WriteLine(i);
+// }
+
+//--------------------------------------______while & do-while loop______---------------
+// Random random = new Random();
+// int current = 0;
+
+// do{
+//     current = random.Next(1,11);
+//     if(current >=8) continue;
+//     Console.WriteLine(current);
+// } while(current != 7);
+
+// while(current !=7){
+//     Console.WriteLine( current);
+//     current = random.Next(1,11);
+// }
+// Console.WriteLine($"Last number: {current}");
+//---------------------------------------------
+// for (int i = 0; i<10; i++){
+//     if (i ==5 || i ==2) continue;
+//     Console.WriteLine( i);
+// }
+
+//--------------------------___________Role playing game battle challenge______-------------
+Random random = new Random();   
+int heroHealth = 20;
+int monsterHealth = 20; 
+int damage ;
+
+do{
+    Console.WriteLine("");
+    damage = random.Next(1, 10);
+    monsterHealth = monsterHealth-damage;
+    Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+
+    if(monsterHealth < 0) continue;
+
+    damage = random.Next(1, 10);
+    heroHealth = heroHealth-damage;
+    Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth} health.");
+
+    Console.WriteLine("");
+} while(heroHealth > 0 && monsterHealth > 0);
+
+if(heroHealth > monsterHealth) Console.WriteLine("Hero Wins!");
+else if(monsterHealth > heroHealth) Console.WriteLine("Monster wins!");
+else Console.WriteLine("Draw x.");
+
+//--------------------------___________Role playing game battle challenge______-------------
+// string? readResult;
+// Console.WriteLine("Enter a string:");
+// do
+// {
+//     readResult = Console.ReadLine();
+// } while (readResult == null);
+//-------valid entry---------
+// string? readResult;
+// bool validEntry = false;
+// Console.WriteLine("Enter a string containing at least three characters:");
+// do
+// {
+//     readResult = Console.ReadLine();
+//     if (readResult != null)
+//     {
+//         if (readResult.Length >= 3)
+//         {
+//             validEntry = true;
+//         }
+//         else
+//         {
+//             Console.WriteLine("Your input is invalid, please try again.");
+//         }
+//     }
+// } while (validEntry == false);
