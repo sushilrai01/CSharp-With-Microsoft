c867a0c5d27d8d3653f43251d8e87bb07353c35c
sushilrai01 <ssilray16@gmail.com>
2023-09-23 15:26:46 +0545

    Convert if-else to Switch

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -134,24 +134,105 @@
 // }
 // Console.WriteLine("Outside of code block: " + value);
 
-int[] numbers = { 4, 8, 15, 16, 23, 42 };
-bool found = false; 
-foreach (int number in numbers)
-{
-    int total = 0;
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+// bool found = false; 
+// foreach (int number in numbers)
+// {
+//     int total = 0;
 
-    total += number;
+//     total += number;
 
-    if (number == 42)
-    {
-       bool found = true;
-        if (found) 
-        {
-            Console.WriteLine("Set contains 42");
+//     if (number == 42)
+//     {
+//        bool found = true;
+//         if (found) 
+//         {
+//             Console.WriteLine("Set contains 42");
 
-        }
-    }
+//         }
+//     }
 
+// }
+
+// Console.WriteLine($"Total: {total}");
+
+// //Switch Statements......
+// int employeeLevel = 100;
+// string employeeName = "John Smith";
+
+// string title = "";
+
+// switch (employeeLevel) 
+// {
+//     // case 100:
+//     // title = "Junior Associate";
+//     // break;
+//     case 100:
+//     case 200:
+    //     title = "Senior Associate";
+    //     break;
+//     case 300:
+    //     title = "Manager";
+    //     break;
+//     case 400:
+    //     title = "Senior Manager";
+    //     break;
+//     default:
+    //     title = "Associate";
+    //     break;
+
+// }
+
+// Console.WriteLine($"{employeeName},{title}");
+//--------------------
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0]) {
+    case "01":
+        type = "Sweat Shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default: 
+        type = "other";
+        break;
 }
 
-Console.WriteLine($"Total: {total}");
+switch (product[1]) {
+    case "BL": 
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default: 
+        color = "White";
+        break;
+}
+
+switch(product[2]){
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
