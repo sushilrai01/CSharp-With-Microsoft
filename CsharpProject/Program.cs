c8b878449a05fc79e5074ddccd5909b0c9cb7bf8
sushilrai01 <ssilray16@gmail.com>
2023-09-30 23:34:18 +0545

    Array methods: Sort, Reverse, Clear, Resize

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -523,25 +523,131 @@
 
 // Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 //____________________________combining string array values as strings and as integers challenge________________
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-decimal number = 0;
-decimal sum = 0;
-bool isNumber = false;
-string str;
-string message = "";
-for (int i = 0; i < values.Length; i++)
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// decimal number = 0;
+// decimal sum = 0;
+// bool isNumber = false;
+// string str;
+// string message = "";
+// for (int i = 0; i < values.Length; i++)
+// {
+//     str = values[i];
+//     isNumber = decimal.TryParse(str, out number);
+//     if (isNumber)
+//     {
+//         sum += number;
+//     }
+//     else
+//     {
+//         message += str;
+//     }
+// }
+
+// Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {sum}");
+
+//
+// int value1 = 12;
+// decimal value2 = 6.2m;
+// float value3 = 4.3f;
+
+// // The Convert class is best for converting the fractional decimal numbers into whole integer numbers
+// //Convert.ToInt32() rounds up the way you would expect.
+// int result1 = Convert.ToInt32((decimal)value1 / value2);
+// Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// decimal result2 = value2 / (decimal)value3;
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// float result3 = value3 / value1;
+// Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+// // float res = (float)value2;
+// // Console.WriteLine(res);
+
+//__________Array Methods: sort,reverse, clear, resize
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+//_______________array: clear_______
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
+
+// Console.WriteLine($"Before: {pallets[0]}");
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"After: {pallets[0]}");
+
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
+
+// Console.WriteLine($"Before: {pallets[0].ToLower()}");
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"After: {pallets[0].ToLower()}");
+
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+//____________________Array.Resize_________________________
+string[] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
 {
-    str = values[i];
-    isNumber = decimal.TryParse(str, out number);
-    if (isNumber)
-    {
-        sum += number;
-    }
-    else
-    {
-        message += str;
-    }
+    Console.WriteLine($"-- {pallet}");
 }
 
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {sum}");
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
