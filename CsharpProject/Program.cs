62c68ecbcee81c282e7131ca359a4d2d9ede14bb
sushilrai01 <ssilray16@gmail.com>
2023-10-02 23:58:31 +0545

    Formatting string: padding and alignment

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -705,25 +705,55 @@
 
 //_______________string formatting basics____________
 //________________ composite formatting________________
-string first = "Hello";
-string second = "World";
-string result = string.Format("{0} {1}!", first, second);
-Console.WriteLine(result);
-//__________String interpolation________________
-decimal prices = 123.45m;
-int discount = 50;
-Console.WriteLine($"Price: {prices:C} (Save {discount:C})");
-//___The N numeric format specifier makes numbers more readable
-decimal measurement = 123456.78912m;
-Console.WriteLine($"Measurement: {measurement:N} units");
-Console.WriteLine($"Measurement: {measurement:N4} units");//precison
-//Formatting Percentages
-decimal tax = .36785m;
-Console.WriteLine($"Tax rate: {tax:P3}");
-//Combining all....
-decimal price = 67.55m;
-decimal salePrice = 59.99m;
+// string first = "Hello";
+// string second = "World";
+// string result = string.Format("{0} {1}!", first, second);
+// Console.WriteLine(result);
+// //__________String interpolation________________
+// decimal prices = 123.45m;
+// int discount = 50;
+// Console.WriteLine($"Price: {prices:C} (Save {discount:C})");
+// //___The N numeric format specifier makes numbers more readable
+// decimal measurement = 123456.78912m;
+// Console.WriteLine($"Measurement: {measurement:N} units");
+// Console.WriteLine($"Measurement: {measurement:N4} units");//precison
+// //Formatting Percentages
+// decimal tax = .36785m;
+// Console.WriteLine($"Tax rate: {tax:P3}");
+// //Combining all....
+// decimal price = 67.55m;
+// decimal salePrice = 59.99m;
 
-string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
-yourDiscount += $"A discount of {((price - salePrice) / price):P2}!"; //inserted
-Console.WriteLine(yourDiscount);
+// string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+// yourDiscount += $"A discount of {((price - salePrice) / price):P2}!"; //inserted
+// Console.WriteLine(yourDiscount);
+
+// print a receipt using string interpolation
+// int invoiceNumber = 1201;
+// decimal productShares = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
+
+// Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// Console.WriteLine($"   Shares: {productShares:N3} Product");
+// Console.WriteLine($"     Sub Total: {subtotal:C}");
+// Console.WriteLine($"           Tax: {taxPercentage:P2}");
+// Console.WriteLine($"     Total Billed: {total:C}");
+//______________Formatting strings by adding whitespace before or after____________________
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadRight(12));
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
