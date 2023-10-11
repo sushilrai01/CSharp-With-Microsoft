94fca047f5bbbcce4521f5cab15b48b4393bc7c0
sushilrai01 <ssilray16@gmail.com>
2023-10-11 23:54:13 +0545

    Identify duplicated code & Create reusable methods

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -944,26 +944,130 @@
 //  _______________________________MEthods in C# ___________________________________
 //       _______________________________________________________________________
 
-int[] a = { 1, 2, 3, 4, 5 };
+// int[] a = { 1, 2, 3, 4, 5 };
 
-Console.WriteLine("Contents of Array:");
-PrintArray();
-//method
-void PrintArray()
+// Console.WriteLine("Contents of Array:");
+// PrintArray();
+// //method
+// void PrintArray()
+// {
+//     foreach (int x in a)
+//     {
+//         Console.Write($"{x} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Before calling a method");
+// SayHello();
+// Console.WriteLine("After calling a method");
+// //method
+
+// void SayHello()
+// {
+//     Console.WriteLine("Hello World!");
+// }
+// //________--Random Numbers--____________
+// Console.WriteLine("Generating random numbers:");
+// DisplayRandomNumbers();
+
+// void DisplayRandomNumbers()
+// {
+//     Random random = new Random();
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         Console.Write($"{random.Next(1, 100)} ");
+//     }
+
+//     Console.WriteLine();
+// }
+
+//_______________________Identify duplicated code & Create reusable methods__________________________
+using System;
+
+int[] times = { 800, 1200, 1600, 2000 };
+int diff = 0;
+
+Console.WriteLine("Enter current GMT");
+int currentGMT = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Current Medicine Schedule:");
+
+/* Format and display medicine times */
+foreach (int val in times)
 {
-    foreach (int x in a)
+    string time = val.ToString();
+    int len = time.Length;
+
+    if (len >= 3)
     {
-        Console.Write($"{x} ");
+        time = time.Insert(len - 2, ":");
     }
-    Console.WriteLine();
+    else if (len == 2)
+    {
+        time = time.Insert(0, "0:");
+    }
+    else
+    {
+        time = time.Insert(0, "0:0");
+    }
+
+    Console.Write($"{time} ");
 }
 
-Console.WriteLine("Before calling a method");
-SayHello();
-Console.WriteLine("After calling a method");
-//method
+Console.WriteLine();
 
-void SayHello()
+Console.WriteLine("Enter new GMT");
+int newGMT = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
 {
-    Console.WriteLine("Hello World!");
+    Console.WriteLine("Invalid GMT");
 }
+else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+{
+    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+
+    /* Adjust the times by adding the difference, keeping the value within 24 hours */
+    for (int i = 0; i < times.Length; i++)
+    {
+        times[i] = ((times[i] + diff)) % 2400;
+    }
+}
+else
+{
+    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+
+    /* Adjust the times by adding the difference, keeping the value within 24 hours */
+    for (int i = 0; i < times.Length; i++)
+    {
+        times[i] = ((times[i] + diff)) % 2400;
+    }
+}
+
+Console.WriteLine("New Medicine Schedule:");
+
+/* Format and display medicine times */
+foreach (int val in times)
+{
+    string time = val.ToString();
+    int len = time.Length;
+
+    if (len >= 3)
+    {
+        time = time.Insert(len - 2, ":");
+    }
+    else if (len == 2)
+    {
+        time = time.Insert(0, "0:");
+    }
+    else
+    {
+        time = time.Insert(0, "0:0");
+    }
+
+    Console.Write($"{time} ");
+}
+
+Console.WriteLine();
