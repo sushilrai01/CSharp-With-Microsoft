dea41ec16eaee51b072cd736b6d73466dfd9f058
sushilrai01 <ssilray16@gmail.com>
2023-10-12 23:04:24 +0545

    Check whether an IPv4 address is valid or invalid

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -984,115 +984,139 @@
 // }
 
 //_______________________Identify duplicated code & Create reusable methods__________________________
-using System;
+// using System;
 
-int[] times = { 800, 1200, 1600, 2000 };
-int diff = 0;
+// int[] times = { 90, 800, 1200, 1600, 2000 };
+// int diff = 0;
 
-Console.WriteLine("Enter current GMT");
-int currentGMT = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter current GMT");
+// int currentGMT = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Current Medicine Schedule:");
+// Console.WriteLine("Current Medicine Schedule:");
+// DisplayTime(times); //function /method
 
-/* Format and display medicine times */
-// foreach (int val in times)
+// Console.WriteLine("Enter new GMT");
+// int newGMT = Convert.ToInt32(Console.ReadLine());
+
+// if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
 // {
-//     string time = val.ToString();
-//     int len = time.Length;
-
-//     if (len >= 3)
-//     {
-//         time = time.Insert(len - 2, ":");
-//     }
-//     else if (len == 2)
-//     {
-//         time = time.Insert(0, "0:");
-//     }
-//     else
-//     {
-//         time = time.Insert(0, "0:0");
-//     }
-
-//     Console.Write($"{time} ");
+//     Console.WriteLine("Invalid GMT");
 // }
-displayTime(times);
+// else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+// {
+//     diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//     AdjustTimes();
 
-Console.WriteLine();
+// }
+// else
+// {
+//     diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//     /* Adjust the times by adding the difference, keeping the value within 24 hours */
+//     AdjustTimes();
 
-Console.WriteLine("Enter new GMT");
-int newGMT = Convert.ToInt32(Console.ReadLine());
+// }
 
-if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+// Console.WriteLine("New Medicine Schedule:");
+// DisplayTime(times);
+
+// //----------------Function
+// void DisplayTime(int[] times)
+// {
+//     foreach (int val in times)
+//     {
+//         string time = val.ToString();
+//         int len = time.Length;
+
+//         if (len >= 3)
+//         {
+//             time = time.Insert(len - 2, ":");
+//         }
+//         else if (len == 2)
+//         {
+//             time = time.Insert(0, "0:");
+//         }
+//         else
+//         {
+//             time = time.Insert(0, "0:0");
+//         }
+
+//         Console.Write($"{time} ");
+//     }
+
+//     Console.WriteLine();
+// }
+
+// void AdjustTimes()
+// {
+//     /* Adjust the times by adding the difference, keeping the value within 24 hours */
+//     for (int i = 0; i < times.Length; i++)
+//     {
+//         times[i] = ((times[i] + diff)) % 2400;
+//     }
+// }
+//_______________________Identify duplicated code & Create reusable methods__________________________
+//   _____________________________________________________________________________________________
+
+//______________________ Checking... whether an IPv4 address is valid or invalid!!__________________________
+using System.Xml.Serialization;
+
+string ipv4Input = "107.031.255.205";
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
+ValidateLength();
+ValidateZeroes();
+ValidateRange();
+
+if (validLength && validZeroes && validRange)
 {
-    Console.WriteLine("Invalid GMT");
-}
-else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-{
-    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-
-    /* Adjust the times by adding the difference, keeping the value within 24 hours */
-    for (int i = 0; i < times.Length; i++)
-    {
-        times[i] = ((times[i] + diff)) % 2400;
-    }
+    Console.WriteLine($"ip is a valid IPv4 address");
 }
 else
 {
-    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-
-    /* Adjust the times by adding the difference, keeping the value within 24 hours */
-    for (int i = 0; i < times.Length; i++)
-    {
-        times[i] = ((times[i] + diff)) % 2400;
-    }
+    Console.WriteLine($"ip is an invalid IPv4 address");
 }
 
-Console.WriteLine("New Medicine Schedule:");
-
-/* Format and display medicine times */
-foreach (int val in times)
+void ValidateLength()
 {
-    string time = val.ToString();
-    int len = time.Length;
-
-    if (len >= 3)
-    {
-        time = time.Insert(len - 2, ":");
-    }
-    else if (len == 2)
-    {
-        time = time.Insert(0, "0:");
-    }
-    else
-    {
-        time = time.Insert(0, "0:0");
-    }
-
-    Console.Write($"{time} ");
+    string[] address = ipv4Input.Split('.');
+    validLength = address.Length == 4;
 }
 
-Console.WriteLine();
-//----------------Function
-void displayTime(int[] times)
+void ValidateRange()
 {
-    foreach (int val in times)
+    string[] address = ipv4Input.Split(".", StringSplitOptions.RemoveEmptyEntries);
+    int number = 0;
+    bool validNo = false;
+    foreach (string str in address)
     {
-        string time = val.ToString();
-        int len = time.Length;
-
-        if (len >= 3)
+        validNo = int.TryParse(str, out number);
+        if (validNo && number > 0 && number < 255)
         {
-            time = time.Insert(len - 2, ":");
-        }
-        else if (len == 2)
-        {
-            time = time.Insert(0, "0:");
+            validRange = true;
         }
         else
         {
-            time = time.Insert(0, "0:0");
+            Console.WriteLine("Greater no.");
+            validRange = false;
+            return; //_________________The return statement terminates execution of the________
+                    //_________________method and returns control to the method caller_________
         }
+        // Console.WriteLine("111 " + str);
+    }
+}
 
-        Console.Write($"{time} ");
+void ValidateZeroes()
+{
+    string[] address = ipv4Input.Split(".");
+    validZeroes = true;
+    foreach (string number in address)
+    {
+        if (number.Length > 1 && number.StartsWith("0"))
+        {
+            Console.WriteLine("Starts with zero'0'");
+            validZeroes = false;
+            return;
+        }
     }
 }
