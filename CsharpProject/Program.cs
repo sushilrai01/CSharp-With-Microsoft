4a4d629a4e32f7b17f1839706b4f102b2212ce9f
sushilrai01 <ssilray16@gmail.com>
2023-10-14 21:26:44 +0545

    RSVP application: named arguments

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -1150,85 +1150,85 @@
 // }
 //_____________________________________________________________________________________________________________
 //________________________________Create a method with parameters_________________________________________
-int[] schedule = { 800, 1200, 1600, 2000 };
+// int[] schedule = { 800, 1200, 1600, 2000 };
 
-void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
-{
-    int diff = 0;
-    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
-    {
-        Console.WriteLine("Invalid GMT");
-    }
-    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-    {
-        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-    }
-    else
-    {
-        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-    }
+// void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+// {
+//     int diff = 0;
+//     if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+//     {
+//         Console.WriteLine("Invalid GMT");
+//     }
+//     else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+//     {
+//         diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//     }
+//     else
+//     {
+//         diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//     }
 
-    for (int i = 0; i < times.Length; i++)
-    {
-        int newTime = ((times[i] + diff)) % 2400;
-        Console.WriteLine($"{times[i]} -> {newTime}");
-    }
-}
+//     for (int i = 0; i < times.Length; i++)
+//     {
+//         int newTime = ((times[i] + diff)) % 2400;
+//         Console.WriteLine($"{times[i]} -> {newTime}");
+//     }
+// }
 
-DisplayAdjustedTimes(schedule, 6, -6);
+// DisplayAdjustedTimes(schedule, 6, -6);
 
-string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
-//______________________________--- Understand method scope ----_____________________
-DisplayStudents(students);
-DisplayStudents(new string[] { "Robert", "Vanya" });
+// string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+// //______________________________--- Understand method scope ----_____________________
+// DisplayStudents(students);
+// DisplayStudents(new string[] { "Robert", "Vanya" });
 
-void DisplayStudents(string[] students)
-{
-    foreach (string student in students)
-    {
-        Console.Write($"{student}, ");
-    }
-    Console.WriteLine();
-}
+// void DisplayStudents(string[] students)
+// {
+//     foreach (string student in students)
+//     {
+//         Console.Write($"{student}, ");
+//     }
+//     Console.WriteLine();
+// }
 
 //______________________________-Parameters passed by value and passed by reference_____________________
 //__________VALUE________________
-int a = 3;
-int b = 4;
-int c = 0;
+// int a = 3;
+// int b = 4;
+// int c = 0;
 
-Multiply(a, b, c);
-Console.WriteLine($"global statement: {a} x {b} = {c}");
+// Multiply(a, b, c);
+// Console.WriteLine($"global statement: {a} x {b} = {c}");
 
-void Multiply(int a, int b, int c)
-{
-    c = a * b;
-    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
-}
-//___________REFERENCE________________
-int[] array = { 1, 2, 3, 4, 5 };
+// void Multiply(int a, int b, int c)
+// {
+//     c = a * b;
+//     Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+// }
+// //___________REFERENCE________________
+// int[] array = { 1, 2, 3, 4, 5 };
 
-PrintArray(array);
-Clear(array);
-PrintArray(array);
+// PrintArray(array);
+// Clear(array);
+// PrintArray(array);
 
-void PrintArray(int[] array)
-{
-    foreach (int a in array)
-    {
-        Console.Write($"{a} ");
-    }
-    Console.WriteLine();
-}
+// void PrintArray(int[] array)
+// {
+//     foreach (int a in array)
+//     {
+//         Console.Write($"{a} ");
+//     }
+//     Console.WriteLine();
+// }
 
-void Clear(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = 0;
-    }
-    Console.WriteLine("cleared!!");
-}
+// void Clear(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = 0;
+//     }
+//     Console.WriteLine("cleared!!");
+// }
 
 //________________________________-Test with strings____________________________________________________ 
 //_________ A new string with the value "Unhealthy" was created and then lost in the method scope.
@@ -1246,14 +1246,60 @@ void Clear(int[] array)
 //     Console.WriteLine($"Middle: {status}");
 // }
 //____________ Overwriting the global status variable with the new string value________________________
-string status = "Healthy";
+// string status = "Healthy";
 
-Console.WriteLine($"Start: {status}");
-SetHealth(false);
-Console.WriteLine($"End: {status}");
+// Console.WriteLine($"Start: {status}");
+// SetHealth(false);
+// Console.WriteLine($"End: {status}");
 
-void SetHealth(bool isHealthy)
+// void SetHealth(bool isHealthy)
+// {
+//     status = (isHealthy ? "Healthy" : "Unhealthy");
+//     Console.WriteLine($"Middle: {status}");
+// }
+//_____________________________ Methods with optional parameters____________________________________________
+// ________________________________Create an RSVP application______________________________________________
+string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+string[] rsvps = new string[10];
+int count = 0;
+
+RSVP("Rebecca", 1, "none", true);
+RSVP("Nadia", 2, "Nuts", true);
+RSVP("Linh", 2, "none", false);
+RSVP("Tony", 1, "Jackfruit", true);
+RSVP("Noor", 4, "none", false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
+
+void RSVP(string name, int partySize, string allergies, bool inviteOnly)
 {
-    status = (isHealthy ? "Healthy" : "Unhealthy");
-    Console.WriteLine($"Middle: {status}");
+    if (inviteOnly)
+    {
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvps[i]);
+    }
 }
