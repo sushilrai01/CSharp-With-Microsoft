c7762c3ac5b7416f9e5a1845fce8d4399f74106a
sushilrai01 <ssilray16@gmail.com>
2023-10-14 22:28:48 +0545

    Challenge to display email addresses

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -1259,54 +1259,87 @@
 // }
 //_____________________________ Methods with optional parameters____________________________________________
 // ________________________________Create an RSVP application______________________________________________
-string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
-string[] rsvps = new string[10];
-int count = 0;
+// string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+// string[] rsvps = new string[10];
+// int count = 0;
 
-// RSVP("Rebecca", 1, "none", true);
-// RSVP("Nadia", 2, "Nuts", true);
-// RSVP("Linh", 2, "none", false);
-// RSVP("Tony", 1, "Jackfruit", true);
-// RSVP("Noor", 4, "none", false);
+// // RSVP("Rebecca", 1, "none", true);
+// // RSVP("Nadia", 2, "Nuts", true);
+// // RSVP("Linh", 2, "none", false);
+// // RSVP("Tony", 1, "Jackfruit", true);
+// // RSVP("Noor", 4, "none", false);
+// // RSVP("Jonte", 2, "Stone fruit", false);
+// RSVP("Rebecca");
+// RSVP("Nadia", 2, "Nuts");
+// RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+// RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+// RSVP("Noor", 4, inviteOnly: false);
 // RSVP("Jonte", 2, "Stone fruit", false);
-RSVP("Rebecca");
-RSVP("Nadia", 2, "Nuts");
-RSVP(name: "Linh", partySize: 2, inviteOnly: false);
-RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
-RSVP("Noor", 4, inviteOnly: false);
-RSVP("Jonte", 2, "Stone fruit", false);
-ShowRSVPs();
+// ShowRSVPs();
 
-void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
-// void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+// void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+// // void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+// {
+//     if (inviteOnly)
+//     {
+//         bool found = false;
+//         foreach (string guest in guestList)
+//         {
+//             if (guest.Equals(name))
+//             {
+//                 found = true;
+//                 break;
+//             }
+//         }
+//         if (!found)
+//         {
+//             Console.WriteLine($"Sorry, {name} is not on the guest list");
+//             return;
+//         }
+//     }
+
+//     rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+//     count++;
+// }
+
+// void ShowRSVPs()
+// {
+//     Console.WriteLine("\nTotal RSVPs:");
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.WriteLine(rsvps[i]);
+//     }
+// }
+// __________________________________Challenge to display email addresses_______________________________
+string[,] corporate =
 {
-    if (inviteOnly)
-    {
-        bool found = false;
-        foreach (string guest in guestList)
-        {
-            if (guest.Equals(name))
-            {
-                found = true;
-                break;
-            }
-        }
-        if (!found)
-        {
-            Console.WriteLine($"Sorry, {name} is not on the guest list");
-            return;
-        }
-    }
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
 
-    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
-    count++;
-}
-
-void ShowRSVPs()
+string[,] external =
 {
-    Console.WriteLine("\nTotal RSVPs:");
-    for (int i = 0; i < count; i++)
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+string username = "";
+string email = "";
+// string externalDomain = "hayworth.com";
+string internalDomain = "contoso.com";
+
+DisplayEmaill(corporate, internalDomain);
+DisplayEmaill(external);
+
+void DisplayEmaill(string[,] employeeTypes, string domain = "hayworth.com")
+
+{
+    for (int i = 0; i < employeeTypes.GetLength(0); i++)
     {
-        Console.WriteLine(rsvps[i]);
+        username = employeeTypes[i, 0].Substring(0, 2) + employeeTypes[i, 1];
+        username = username.ToLower();
+        email = $"{username}@{domain}";
+        Console.WriteLine(email);
     }
+    Console.WriteLine("__________________");
 }
