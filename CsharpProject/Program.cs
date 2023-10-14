3002acd11d06b9d62f14076117b5b4893e765148
sushilrai01 <ssilray16@gmail.com>
2023-10-14 21:38:59 +0545

    RSVP application: optional arguments

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -1263,15 +1263,22 @@ string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
 string[] rsvps = new string[10];
 int count = 0;
 
-RSVP("Rebecca", 1, "none", true);
-RSVP("Nadia", 2, "Nuts", true);
-RSVP("Linh", 2, "none", false);
-RSVP("Tony", 1, "Jackfruit", true);
-RSVP("Noor", 4, "none", false);
+// RSVP("Rebecca", 1, "none", true);
+// RSVP("Nadia", 2, "Nuts", true);
+// RSVP("Linh", 2, "none", false);
+// RSVP("Tony", 1, "Jackfruit", true);
+// RSVP("Noor", 4, "none", false);
+// RSVP("Jonte", 2, "Stone fruit", false);
+RSVP("Rebecca");
+RSVP("Nadia", 2, "Nuts");
+RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+RSVP("Noor", 4, inviteOnly: false);
 RSVP("Jonte", 2, "Stone fruit", false);
 ShowRSVPs();
 
-void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+// void RSVP(string name, int partySize, string allergies, bool inviteOnly)
 {
     if (inviteOnly)
     {
