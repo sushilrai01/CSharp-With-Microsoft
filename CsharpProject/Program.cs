7a393228cbcf811df0a7099086071cbeecdd3857
sushilrai01 <ssilray16@gmail.com>
2023-10-15 22:15:21 +0545

    methods that return values: boolean, arrays

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -1403,35 +1403,107 @@
 // }
 
 // __________________________________ Return strings from methods(reverse a string without using string.Reverse)__________________________________ 
-string input = "Ya Hoo!";
-string reverseString = ReverseWord(input);
-Console.WriteLine(reverseString);
+// string input = "Ya Hoo!";
+// string reverseString = ReverseWord(input);
+// Console.WriteLine(reverseString);
 
-string ReverseWord(string word)
-{
-    string result = "";
-    for (int i = word.Length - 1; i >= 0; i--)
-    {
-        result += word[i];
-    }
-    return result;
-}
+// string ReverseWord(string word)
+// {
+//     string result = "";
+//     for (int i = word.Length - 1; i >= 0; i--)
+//     {
+//         result += word[i];
+//     }
+//     return result;
+// }
 
 // _________________________________ Method to reverse words in a sentence_________________________________
 
-string sentence = "You can extract the individual words from the string using string.Split() ";
+// string sentence = "You can extract the individual words from the string using string.Split() ";
 
-Console.WriteLine(sentence);
-Console.WriteLine(ReverseSentence(sentence));
+// Console.WriteLine(sentence);
+// Console.WriteLine(ReverseSentence(sentence));
 
-string ReverseSentence(string input)
+// string ReverseSentence(string input)
+// {
+//     string result = "";
+//     string[] words = input.Split(" ");
+//     foreach (string word in words)
+//     {
+//         result += ReverseWord(word) + " ";
+//     }
+//     return result.Trim();
+// }
+// // _________________________________Method that returns a Boolean:IsPalindrome?-_________________________________
+
+// string[] words = { "racecar", "talented", "deified", "tent", "tenet" };
+
+// Console.WriteLine("Is it a palindrome?");
+// foreach (string word in words)
+// {
+//     Console.WriteLine($"{word}: {IsPalindrome(word)}");
+// }
+
+// bool IsPalindrome(string word)
+// {
+//     int start = 0;
+//     int end = word.Length - 1;
+
+//     while (start < end)
+//     {
+//         if (word[start] != word[end])
+//         {
+//             return false;
+//         }
+//         start++;
+//         end--;
+
+//     }
+
+//     return true;
+// }
+// ______________________________________-Return arrays from methods-___________________________________
+int target = 30;
+int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[,] result = TwoCoins(coins, target);
+
+if (result.Length == 0)
 {
-    string result = "";
-    string[] words = input.Split(" ");
-    foreach (string word in words)
+    Console.WriteLine("No two coins make change");
+}
+else
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < result.GetLength(0); i++)
     {
-        result += ReverseWord(word) + " ";
+        if (result[i, 0] == -1)
+        {
+            break;
+        }
+        Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
     }
-    return result.Trim();
 }
 
+int[,] TwoCoins(int[] coins, int target)
+{
+    int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+    int count = 0;
+
+    for (int curr = 0; curr < coins.Length; curr++)
+    {
+        for (int next = curr + 1; next < coins.Length; next++)
+        {
+            if (coins[curr] + coins[next] == target)
+            {
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
+            }
+            if (count == result.GetLength(0))
+            {
+                return result;
+            }
+        }
+    }
+    return (count == 0) ? new int[0, 0] : result;
+}
