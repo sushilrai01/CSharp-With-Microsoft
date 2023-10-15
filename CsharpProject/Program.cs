e17ea9661e202d5b5a29be9c0fb0d3936613e248
sushilrai01 <ssilray16@gmail.com>
2023-10-15 23:00:05 +0545

    Dice mini-game challenge

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -1463,47 +1463,106 @@
 //     return true;
 // }
 // ______________________________________-Return arrays from methods-___________________________________
-int target = 30;
-int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
-int[,] result = TwoCoins(coins, target);
+// int target = 30;
+// int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+// int[,] result = TwoCoins(coins, target);
 
-if (result.Length == 0)
+// if (result.Length == 0)
+// {
+//     Console.WriteLine("No two coins make change");
+// }
+// else
+// {
+//     Console.WriteLine("Change found at positions:");
+//     for (int i = 0; i < result.GetLength(0); i++)
+//     {
+//         if (result[i, 0] == -1)
+//         {
+//             break;
+//         }
+//         Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
+//     }
+// }
+
+// int[,] TwoCoins(int[] coins, int target)
+// {
+//     int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+//     int count = 0;
+
+//     for (int curr = 0; curr < coins.Length; curr++)
+//     {
+//         for (int next = curr + 1; next < coins.Length; next++)
+//         {
+//             if (coins[curr] + coins[next] == target)
+//             {
+//                 result[count, 0] = curr;
+//                 result[count, 1] = next;
+//                 count++;
+//             }
+//             if (count == result.GetLength(0))
+//             {
+//                 return result;
+//             }
+//         }
+//     }
+//     return (count == 0) ? new int[0, 0] : result;
+// }
+//__________________________________________________________________________________________________
+//__________________________________Dice mini-game challenge_____________________________________________
+//__________________________________________________________________________________________________
+using System.Reflection.Metadata;
+
+Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay())
 {
-    Console.WriteLine("No two coins make change");
+    PlayGame();
 }
-else
+
+bool ShouldPlay()
 {
-    Console.WriteLine("Change found at positions:");
-    for (int i = 0; i < result.GetLength(0); i++)
+    string readResult = Console.ReadLine();
+    return readResult.ToLower().Equals("y");
+}
+
+void PlayGame()
+{
+    var play = true;
+
+    while (play)
     {
-        if (result[i, 0] == -1)
-        {
-            break;
-        }
-        Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
+        int target = GetTarget();
+        int roll = RollDice();
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose(roll, target));
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
     }
 }
 
-int[,] TwoCoins(int[] coins, int target)
+int GetTarget()
 {
-    int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
-    int count = 0;
+    return random.Next(1, 6);
+}
 
-    for (int curr = 0; curr < coins.Length; curr++)
+int RollDice()
+{
+    return random.Next(1, 7);
+}
+
+string WinOrLose(int roll, int target)
+{
+    if (roll > target)
     {
-        for (int next = curr + 1; next < coins.Length; next++)
-        {
-            if (coins[curr] + coins[next] == target)
-            {
-                result[count, 0] = curr;
-                result[count, 1] = next;
-                count++;
-            }
-            if (count == result.GetLength(0))
-            {
-                return result;
-            }
-        }
+        return "You win!";
     }
-    return (count == 0) ? new int[0, 0] : result;
+    else if (roll == target)
+    {
+        return "Draw!";
+    }
+    return "You lose!!";
 }
